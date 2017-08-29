c7bbebd9784ba5fedda01eb527651abf7be24028
Michelle Biederman <mbieder2@jhu.edu>
2017-08-29 19:25:37 -0400

    homework 2 added

--- day2-afternoon/00-iteration.py
+++ day2-afternoon/00-iteration.py
@@ -10,10 +10,13 @@ f = open( sys.argv[1] )
         #break
     #print the_line
     
-my_iter = iter( f )
-while True:
-    the_line = my_iter.next()
-    print the_line
+#my_iter = iter( f )
+#while True:
+    #the_line = my_iter.next()
+    #print the_line
+    
+for line in f:
+    print line    
     
     
         
