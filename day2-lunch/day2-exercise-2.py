54b0b2cea07a8e832d83ef018a93b0d10e310abe
Michelle Biederman <mbieder2@jhu.edu>
2017-08-29 14:01:17 -0400

    day2 exercise 2 added

---
+++ day2-lunch/day2-exercise-2.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+import sys
+
+# Opening file with open
+## f = open( "/Users/cmdb/data/genomes/BDGP6.fa")
+
+## f = open ( sys.argv[1] )
+count = 0
+
+if len( sys.argv ) > 1:
+    sam = open( sys.argv[1] )   
+else:
+    sam = sys.stdin
+  
+        
+for line in sam:
+    if line.startswith("@"):
+        n = 0
+    else:
+        if "NM:i:0" in line:
+            n=1
+        else:
+            n=0
+    count = count + n              
+
+print count
