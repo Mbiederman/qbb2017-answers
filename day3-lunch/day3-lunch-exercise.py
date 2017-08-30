f706b89e986c31bbc699d9c5b17ac8d0fd4ed022
Michelle Biederman <mbieder2@jhu.edu>
2017-08-30 13:51:23 -0400

    added day3-lunch.py

---
+++ day3-lunch/day3-lunch-exercise.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+
+import random
+
+key = 4
+#initialize the whole to be searched
+nums = range(10)
+
+hi = len(nums)     
+lo = 0
+
+#main loop: keep going while there are options available
+while lo < hi:
+    midix = (hi + lo)/2
+    mid = nums[midix]
+    print "checking in the range [%d, %d] midix[%d] = %d" % (lo, hi, midix, mid)
+    if (mid == key):
+        print "found %d, at position %d, who's value is %d!" % (key, midix, mid)
+        break
+    elif key > mid:
+        lo = midix +1
+    else:
+        hi = midix
+    
+                   
+#print nums 
+
+
+#for i in nums:
+   # while key != i:
+       # if key > (len(nums)/2):
+           # nums = nums.range(len(nums/2),10)
+       # else:
+           # nums = nums.range(0,(len(nums/2))
+#    print nums
+            
+
