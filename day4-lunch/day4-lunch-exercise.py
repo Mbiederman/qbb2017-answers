48eeaa7e3c4fa40687abbe8d72e82d63071d98e5
Michelle Biederman <mbieder2@jhu.edu>
2017-08-31 14:40:39 -0400

    edited and broken

--- day4-lunch/day4-lunch-exercise.py
+++ day4-lunch/day4-lunch-exercise.py
@@ -12,15 +12,15 @@ fpkm= ["FPKM"]
 
 
 
-x = np.log(df1[fpkm]+1)
-y = np.log(df2[fpkm]+1)
+x = np.log(df1[fpkm] + 1)
+y = np.log(df2[fpkm] + 1)
 ##np.polyfit(df1.values.flatten(), df2.values.flatten(), 1)
 #poly = np.polyfit(x,y,1)
 #fit = np.poly1d(poly)
 
 plt.figure()
 plt.scatter( x, y, alpha=0.05)
-#plt.plot(poly, fit)
+plt.plot(np.unique(x), np.poly1d(np.polyfit(x,y, deg=2))(np.unique(x)))
 plt.axis([0, 10, 0, 10])
 plt.grid(True)
 plt.xlabel("SRR072915")
